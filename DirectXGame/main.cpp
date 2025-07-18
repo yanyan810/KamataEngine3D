b4a35b7aeb0aad4ab0f836a8167877e3845d07f3
yanyan0315 <k024g0064@g.neec.ac.jp>
2025-07-18 14:16:10 +0900

    いったんこみ

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,88 +4,14 @@
 #include "TitleScene.h"
 using namespace KamataEngine;
 
-enum class Scene {
-
-	kUnknown=0, // 不明なシーン
-
-	kTitle, // タイトルシーン
-	kGame,  // ゲームシーン
-};
 
 
-Scene scene = Scene::kUnknown; // 現在のシーン
 
 GameScene* gameScene = nullptr; // ゲームシーンのインスタンス
 
-TitleScene* titleScene = nullptr;
-
-void ChangeScene() {
-	switch (scene) {
-	
-	case Scene::kTitle:
-		if (titleScene->IsFinished()) {
-		//シーン変更
-			scene = Scene::kGame; // タイトルシーンが終了したらゲームシーンに変更
-
-			delete titleScene; // タイトルシーンのインスタンスを解放
-			titleScene = nullptr;
-			// ゲームシーンの初期化
-			                      // ゲームシーンのインスタンス生成
-			gameScene = new GameScene();
-			// ゲームシーンの初期化
-			gameScene->Initialize();
-		} 
-
-		break;
-	case Scene::kGame:
-
-		if (gameScene->IsFinished()) {
-			// シーン変更
-			scene = Scene::kTitle; // ゲームシーンが終了したらタイトルシーンに変更
-			delete gameScene;      // ゲームシーンのインスタンスを解放
-			gameScene = nullptr;
-			// タイトルシーンの初期化
-			titleScene = new TitleScene();
-			titleScene->Initialize();
-		}
-
-
-		break;
-	default:
-		break;
-	}
 
 
 
-}
-
-void UpdataScene() {
-	switch (scene) {
-	case Scene::kTitle:
-		// タイトルシーンの更新
-		titleScene->Update();
-		break;
-	case Scene::kGame:
-		// ゲームシーンの更新
-		gameScene->Update();
-		break;
-
-
-	}
-}
-
-void DrawScene() {
-	switch (scene) {
-	case Scene::kTitle:
-		// タイトルシーンの描画
-		titleScene->Draw();
-		break;
-	case Scene::kGame:
-		// ゲームシーンの描画
-		gameScene->Draw();
-		break;
-	}
-}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -99,9 +25,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
 
 	
-	
-
-	scene = Scene::kTitle; // シーンをタイトルに設定
+gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();	
 
 	/*#ifdef _DEBUG
 	scene = Scene::kGame;
@@ -115,9 +41,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	titleScene->Initialize();
 #endif*/
 	
-	titleScene = new TitleScene();
-	// タイトルシーンの初期化
-	titleScene->Initialize();
+
 
 
 	//メインループ
@@ -132,9 +56,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		
 
-		ChangeScene();
-		UpdataScene();
-
+	
 		
 
 		//ImGui受付終了
@@ -143,7 +65,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//描画開始
 		dxCommon->PreDraw();
 
-		DrawScene();
 		
 		//軸表示の描画
 		AxisIndicator::GetInstance()->Draw();
@@ -162,7 +83,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームシーンの解放
 	delete gameScene;
-	delete titleScene;
+	
 	//nullptrの代入
 	gameScene = nullptr;
 
