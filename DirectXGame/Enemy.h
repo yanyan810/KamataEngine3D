152e2694d4db66dd3c64db14f5fd53de1d78214d
yanyan0315 <k024g0064@g.neec.ac.jp>
2025-07-21 15:48:34 +0900

    敵の行動パターンの遷移

--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -5,7 +5,15 @@
 #include "Easing.h"
 #include "Operator.h"
 
+	enum class Phase {
+	Approach, // 接近する
+	Leave,    // 離脱する
+	Unknown,
+};
+
 class Enemy {
+
+
 
 public:
 	void Initialize(KamataEngine::Model* model,const KamataEngine::Vector3& position, uint32_t textureHandle);
@@ -14,10 +22,16 @@ public:
 
 	void Draw(const KamataEngine::Camera& camera);
 
+	void ApproachUpdate();
+
+	void LeaveUpdate();
+
 	private:
 
 		KamataEngine::WorldTransform worldTransform_;
 	    KamataEngine::Model* model_ = nullptr;
 	    uint32_t textureHandle_ = 0;
+
+		Phase phase_ = Phase::Unknown;
 
 };
